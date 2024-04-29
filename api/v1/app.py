79175ad5c7a896396b85a2711c8d78fdfb4108a1
sanzamcmillian <g18s2658@ru.ac.za>
2024-04-29 11:27:39 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,40 @@
 #!/usr/bin/python3
 """ A Flask API """
 import flask from Flask
+from flask_cors import CORS
+from os import getenv
 from models import storage
 from api.v1.views import api_views
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 """ Register blueprints """
 app.register_blueprint(app_views)
 
 
-""" Method to handle teardown_appcontext """
-@app.teardown_appcontext(exception):
+@app.teardown_appcontext
+def teardown(exception):
+    """ Method to handle teardown_appcontext """
     storage.close() 
 
+@app.errorhandler(404)
+def handle_404(exception):
+    """handles 404 erro
 
+    Args:
+        exception (_type_): param.
+        return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+    
+    resp = jsonify(data)
+    resp.status_code = 404
+    
+    return(resp)
 
 if __name__ == "__main__":
-    host = os.env.get("HBNB_API_HOST", "0.0.0.0"
-    port = int(os.env.get("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
