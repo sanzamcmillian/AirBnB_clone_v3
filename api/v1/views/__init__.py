9a6339747d0d38d284b9dd6e5ebe01b8fb0cf6a5
sanzamcmillian <g18s2658@ru.ac.za>
2024-04-30 11:12:00 +0200

    task 6

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,5 +2,6 @@
 """ Create flask app blueprint """
 from flask import Blueprint
 from api.v1.views.index import * 
+from api.v1.views.states import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
