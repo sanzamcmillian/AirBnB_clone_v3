ed9e38ad81119451041771080ae0e30ed221f6bd
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-05-01 09:04:10 -0700

    get and coutn method updates

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,47 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing file storage")
+    def test_get(self):
+        """ Test method for getting an instance of db storage """
+        storage = FileStorage()
+        storage.reload()
+
+        state_data = {"name": "Dakar"}
+        state_instance = State(**state_data)
+        storage.new(state_instance)
+
+        storage.save()
+
+        returned_state = storage.get(State, state_instance.id)
+
+        self.assertEqual(state_instance, returned_state)
+
+        null_state_id = storage.get(State, 'null_id')
+
+        self.assertEqual(null_state_id, None)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing file storage")
+    def test_count(self):
+        """ Test for getting an instance of db storage """
+        storage = FileStorage()
+        storage.reload()
+
+        state_data = {"name": "Benifica"}
+        state_instance = State(**state_data)
+        storage.new(state_instance)
+
+        city_data = {"name": "Cali", "state_id": state_instance.id}
+
+        city_instance = City(**city_data)
+
+        storage.new(city_instance)
+
+        storage.save()
+
+        state_count = storage.count(State)
+        self.assertEqual(state_count, len(storage.all(State)))
+
+        all_state_count = storage.count()
+        self.assertEqual(all_state_count, len(storage.all()))
