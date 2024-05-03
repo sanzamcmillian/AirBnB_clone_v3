999423faa798515d0b56d968e00d1c30ecd092de
sanzamcmillian <g18s2658@ru.ac.za>
2024-05-03 16:23:26 +0200

    5

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,7 +9,7 @@ from models.state import State
 
 
 @app_views.route("/states", methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_state.yml', methods=False)
+@swag_from('documentation/state/get_state.yml', methods=['GET'])
 def state_get_all():
     """retrieves all state objects
        return: json of all states
@@ -17,12 +17,11 @@ def state_get_all():
     state_list = []
     state_obj = storage.all(State).values()
     for state in state_obj:
-        state_list.append(state.to_dict())
-        
+        state_list.append(state.to_dict())    
     return jsonify(state_list)
 
 
-@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/state/get_id_state.yml', methods=['get'])
 def state_by_id(state_id):
     """gets a specific state object by ID
@@ -31,13 +30,11 @@ def state_by_id(state_id):
         state_id (_type_): state object id
         return: state obj with the specified id or erro
     """
-    
     fetched_obj = storage.get(State, state_id)
     
     if not fetched_obj:
-        abort(404)
-        
-    return make_response(jsonify({}), 200)
+        abort(404)    
+    return jsonify(fetched_obj.to_dict())
 
 
 @app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
@@ -60,7 +57,7 @@ def state_put(state_id):
     
     data = request.get_json()
     for key, value in data.items():
-        if kry not in ignore:
+        if key not in ignore:
             setattr(State, key, value)
     storage.save()
     return make_response(jsonify(state.to_dict()), 200)
